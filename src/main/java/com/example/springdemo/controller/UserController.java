905949cfc4945768dc6aa98a2823ac98efd27c83
samve7777 <boob.2012@mail.ru>
2020-10-13 16:22:22 +0300

    SpringDemo

--- src/main/java/com/example/springdemo/controller/UserController.java
+++ src/main/java/com/example/springdemo/controller/UserController.java
@@ -1,12 +1,9 @@
 package com.example.springdemo.controller;
 
 import com.example.springdemo.model.User;
+import com.example.springdemo.service.EmailService;
 import com.example.springdemo.service.UserService;
 import lombok.RequiredArgsConstructor;
-//import org.apache.commons.io.IOUtils;
-//import org.springframework.beans.factory.annotation.Value;
-//import org.springframework.http.MediaType;
-//import org.springframework.security.crypto.password.PasswordEncoder;
 import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
@@ -20,11 +17,7 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Optional;
-
-//import java.io.File;
-//import java.io.FileInputStream;
-//import java.io.IOException;
-//import java.io.InputStream;
+import java.util.UUID;
 
 @Controller
 @RequiredArgsConstructor
@@ -35,20 +28,44 @@ public class UserController {
 
     private final UserService userService;
     private final PasswordEncoder passwordEncoder;
+    private final EmailService emailService;
 
     @PostMapping("/addUser")
     public String addUser(@ModelAttribute User user, @RequestParam("image") MultipartFile file) throws IOException {
+        if (!user.getPassword().equals(user.getConfirmPassword())) {
+            return "redirect:/?msg=Password and ConfirmPassword dase note match!!!";
+        }
         Optional<User> byUsername = userService.findByUsername(user.getUsername());
         if (byUsername.isPresent()) {
-            return "redirect:/?msg=User already exists";
+            return "redirect:/?msg=User already exists!!!";
         }
         String name = System.currentTimeMillis() + "_" + file.getOriginalFilename();
         File image = new File(uploadDir, name);
         file.transferTo(image);
         user.setProfilePic(name);
         user.setPassword(passwordEncoder.encode(user.getPassword()));
+        user.setActive(false);
+        user.setToken(UUID.randomUUID().toString());
         userService.save(user);
+        String link = "http://localhost:8081/activate?email=" + user.getUsername() + "&token=" + user.getToken();
+        emailService.send(user.getUsername(),
+                "Wlecome", "Dear " + user.getName() + " You have successfully registered. Please activate your account by clicking on: ->" + link);
         return "redirect:/?msg=User was added";
+    }
+
+    @GetMapping("/activate")
+    public String activate(@RequestParam("email") String username, @RequestParam("token") String token) {
+        Optional<User> byUsername = userService.findByUsername(username);
+        if (byUsername.isPresent()) {
+            User user = byUsername.get();
+            if (user.getToken().equals(token)) {
+                user.setActive(true);
+                user.setToken("");
+                userService.save(user);
+                return "redirect:/?msg=User was activate. Please Login";
+            }
+        }
+        return "redirect:/?msg=Something went wrong. Please try again!!!";
     }
 
     @GetMapping("/deleteUser")
@@ -59,7 +76,8 @@ public class UserController {
     }
 
     @GetMapping(value = "/image", produces = MediaType.IMAGE_JPEG_VALUE)
-    public @ResponseBody byte[] getImage(@RequestParam("name") String imageName) throws IOException {
+    public @ResponseBody
+    byte[] getImage(@RequestParam("name") String imageName) throws IOException {
         InputStream in = new FileInputStream(uploadDir + File.separator + imageName);
         return IOUtils.toByteArray(in);
     }
